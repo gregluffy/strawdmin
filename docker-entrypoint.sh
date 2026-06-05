f4de3badec5f593c2ac1402b778dc1cbaaccbe2c
Grigoris Fountopoulos <gregluffy@gmail.com>
2026-06-05 22:31:54 +0300

    fix: update placeholder path in docker-entrypoint.sh to match project name

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 chown -R node:node /app/data
 
-PLACEHOLDER="/openadmin-base-path-placeholder"
+PLACEHOLDER="/strawdmin-base-path-placeholder"
 RUNTIME_BASE="${BASE_PATH:-}"
 
 find /app/.next -type f \( -name "*.js" -o -name "*.json" -o -name "*.rsc" -o -name "*.html" \) -print0 | xargs -0 sed -i "s|$PLACEHOLDER|$RUNTIME_BASE|g"
