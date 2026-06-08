fda915dde11357e84e0a4537fb1ffbe6b8da452b
Grigoris Fountopoulos <gregluffy@gmail.com>
2026-06-08 22:07:43 +0300

    fix: standardize access to BASE_PATH using bracket notation

--- lib/api-url.ts
+++ lib/api-url.ts
@@ -1,4 +1,4 @@
 export const basePath =
   typeof window !== "undefined"
     ? ((window as any).__NEXT_BASE_PATH__ ?? "")
-    : (process.env.BASE_PATH ?? "");
+    : (process.env["BASE_PATH"] ?? "");
